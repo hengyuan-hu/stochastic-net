e2b1c2a65283abf88da1b9c0a4a334132517e673
hengyuan-hu <kevin.huhy@gmail.com>
2016-10-20 17:14:15 -0400

    oh fuck

--- customized_layers.py
+++ customized_layers.py
@@ -6,24 +6,19 @@ import numpy as np
 class GateLayer(Layer):
     def __init__(self, skip_rate, **kwargs):
         """Return a * squared(x), a is a learnable scalar."""
-        self.skip_rate = skip_rate
+        self.skip_rate = K.variable(np.array([skip_rate]))
         super(GateLayer, self).__init__(**kwargs)
 
     def call(self, x, mask=None):
         """[:batch] is for conv, [batch:] is for identity."""
-        p = np.random.normal(0, 1, (1,))[0]
+        p = K.random_uniform((1,))
         zeros = K.zeros_like(x)
-        # print x.get_shape().as_list()
-        # print x._keras_shape
-        # print zeros.get_shape().as_list()
 
         skip_out = K.concatenate([zeros, x], axis=1)
         no_skip_out = K.concatenate([x, zeros], axis=1)
-        if p < self.skip_rate:
-            # skip
-            return K.in_train_phase(skip_out, skip_out)
-        else:
-            return K.in_train_phase(no_skip_out, no_skip_out)
+        return K.switch(K.lesser(p[0], self.skip_rate[0]),
+                        K.in_train_phase(skip_out, no_skip_out),
+                        K.in_train_phase(no_skip_out, no_skip_out))
 
     def get_output_shape_for(self, input_shape):
         # input shape: (batch, ...)
